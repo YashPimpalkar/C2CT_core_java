75d3a8fdc88f6476fdc292515e136c133b8eca9f
YashPimpalkar <vu1s2223002@pvppcoe.ac.in>
2024-04-08 15:28:49 +0530

    comment

--- TNSIF/src/com/tnsif/daythree/Executor.java
+++ TNSIF/src/com/tnsif/daythree/Executor.java
@@ -1,5 +1,6 @@
 package com.tnsif.daythree;
 
+// Program is on "Has a " relationship implemented
 public class Executor {
     public static void main(String[] args) {
     	Address a=new Address("New Mill road","near market","kurla");
